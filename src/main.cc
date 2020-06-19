3f5fe53c2de094659f23a72e04e86978a6ee69b4
Luis Nogoseke <nogoseke42@gmail.com>
2020-06-19 11:06:36 -0300

    Simple CMake project

---
+++ src/main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world, this is venko-example/n";
+    return 0;
+}
---
+++ tests/basic_tests.cc
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(Basic, Test) {
+  EXPECT_TRUE(true);
+}
